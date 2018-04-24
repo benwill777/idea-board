411d48a8189f79d27a832696d38b1b3316c029ae
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-04-24 14:07:41 -0400

    changed app.get to app.use in server.js

--- server.js
+++ server.js
@@ -19,9 +19,12 @@ connection.on('error', (err) => {
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/client/build/'));
 
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/build/index.html')
 })
+const UsersController = require('./controllers/users')
+app.use('/api/users', UsersController)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
