4dc29718cd34782fb70bd732d85cfb4d24fcaf04
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-04-24 14:27:33 -0400

    added redirect buttons from home to login and login to home

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class HomePage
     extends Component {
     render() {
         return (
             <div>
-                Home Page
+                <Link to='/login'> <button>Login Here</button></Link>
             </div>
         );
     }
--- client/src/components/LogInPage.js
+++ client/src/components/LogInPage.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom'
 class LoginPage extends Component {
     render() {
         return (
             <div>
-                Log In Page
+                <Link to='/'> <button>Go Home </button></Link>
             </div>
         );
     }
