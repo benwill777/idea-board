641ac4ee97ba62961c36abc092766c62f92d0a30
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-04-24 14:12:39 -0400

    added top three components

---
+++ client/src/components/LogInPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <div>
+                Log In Page
+            </div>
+        );
+    }
+}
+
+export default LoginPage;
